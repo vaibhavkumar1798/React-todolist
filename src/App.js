96723c3e83306180ffc4d25a625a85b1ec554895
Vaibhav <vaibhavkumar1798@gmail.com>
2023-07-20 11:05:14 +0530

    project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Todo from "./Components/Todo";
+
+function App() {
+  return (
+    <div className="container">
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
+
